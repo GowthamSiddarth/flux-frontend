0e08e2da70eea98d22957679d5e0b8e8a32f317c
Gowtham Behara <gothsiddu@gmail.com>
2018-10-04 17:33:01 +0530

    Setup webpack-dev-server with webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'main.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        compress: true,
+        port: 9000
+    },
     module: {
         rules: [
             {
@@ -32,6 +44,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(['dist/*'], { root: path.join(__dirname, "dist/*"), beforeEmit: true }),
         new HtmlWebpackPlugin({
             template: "./src/index.html",
             filename: "./main.html"
